dff9b27326d7fad762e6b5c4ba4c529db5acc480
Alexis <ferguanoluisa99@hotmail.com>
2023-11-13 14:39:27 -0500

    primer commit deber

---
+++ src/main/java/com/uce/edu/test/Pa2TestP5AgApplication.java
@@ -0,0 +1,20 @@
+package com.uce.edu.test;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2TestP5AgApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2TestP5AgApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Tarea de Git N°1");
+		System.out.println("Alexis Guanoluisa");
+	}
+
+}
---
+++ src/test/java/com/uce/edu/test/Pa2TestP5AgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pa2TestP5AgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
